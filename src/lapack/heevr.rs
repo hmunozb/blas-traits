40c1b80a96dce2a0218b786562ffa11b2f6a125d
hmb <humberto.munozb@gmail.com>
2020-04-20 02:40:22 -0700

    Added heevr

--- src/lapack/heevr.rs
+++ src/lapack/heevr.rs
@@ -4,7 +4,6 @@ use num_complex::Complex32 as c32;
 use num_complex::Complex64 as c64;
 
 pub trait Theevr: ComplexField{
-
     unsafe fn heevr(layout: Layout,
         jobz: u8,
         range: u8,
@@ -16,7 +15,7 @@ pub trait Theevr: ComplexField{
         vu: Self::RealField,
         il: i32,
         iu: i32,
-        abstol: f32,
+        abstol: Self::RealField,
         m: &mut i32,
         w: &mut [Self::RealField],
         z: &mut [Self],
@@ -24,3 +23,37 @@ pub trait Theevr: ComplexField{
         isuppz: &mut [i32],
     ) -> i32;
 }
+
+macro_rules! impl_heevr (
+    ($N: ty, $heevr: path) => (
+        impl Theevr for $N{
+            #[inline]
+            unsafe fn heevr(layout: Layout,
+                jobz: u8,
+                range: u8,
+                uplo: u8,
+                n: i32,
+                a: &mut [Self],
+                lda: i32,
+                vl: Self::RealField,
+                vu: Self::RealField,
+                il: i32,
+                iu: i32,
+                abstol: Self::RealField,
+                m: &mut i32,
+                w: &mut [Self::RealField],
+                z: &mut [Self],
+                ldz: i32,
+                isuppz: &mut [i32],
+            ) -> i32{
+                $heevr(layout, jobz, range, uplo, n, a, lda, vl, vu, il, iu, abstol,
+                       m, w, z, ldz, isuppz)
+            }
+        }
+    )
+);
+
+impl_heevr!(f32, ssyevr);
+impl_heevr!(f64, dsyevr);
+impl_heevr!(c32, cheevr);
+impl_heevr!(c64, zheevr);
